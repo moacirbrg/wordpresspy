c888c6f1ff1cb49e77e2d693b6ce0fb300bf7ffb
Moacir Braga <contato@moacirbrg.org>
2018-05-21 22:52:40 -0300

    Required files to make a package

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='wordpresspy',
+      version='0.0.1',
+      description='Library for management WordPress and its plugins',
+      url='https://github.com/weecan-group/wordpresspy',
+      author='Weecan',
+      author_email='contato@moacirbrg.org',
+      license='MIT',
+      packages=['wordpresspy'],
+      zip_safe=False,
+      classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+      ))
---
+++ wordpresspy/__init__.py
@@ -0,0 +1,2 @@
+from .api import API
+from .http_method import HTTP_DELETE, HTTP_GET, HTTP_POST, HTTP_PUT
