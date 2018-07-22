a8a166eecd1e0c133700b09d05c309b59ba25315
Moacir Braga <contato@moacirbrg.org>
2018-07-21 22:18:23 -0300

    Version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='wordpresspy',
-      version='0.0.11',
+      version='0.0.12',
       long_description='Library for management WordPress and its plugins, ' +
                        'please visit ' +
                        'https://github.com/weecan-group/wordpresspy for ' +
